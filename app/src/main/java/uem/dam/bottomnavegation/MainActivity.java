6b1b9050b409e62f9a2c83f3b52e1fe5e1537614
yago <yago.igle@gmail.com>
2020-05-14 12:11:38 +0200

    Commit Prueba4

--- app/src/main/java/uem/dam/bottomnavegation/MainActivity.java
+++ app/src/main/java/uem/dam/bottomnavegation/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
     BottomNavigationView mBottomNavigation;
     FusedLocationProviderClient flClient;
     Location miLoc;
-    //Hola
+    //prueba
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
